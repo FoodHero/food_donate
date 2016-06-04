426ed71fa5ea99170982ba0987cf75fc6681dfa3
paulcarroll <pvcarroll@gmail.com>
2016-06-04 18:11:06 -0500

    Donation seed data, display in pages/index.html.erb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+donees = Donee.create([
+                      {name: 'donee1'},
+                  ])
+
+donors = Donor.create([
+                         {name: 'donor1'},
+                     ])
+
+donations = Donation.create([
+                              {donor_id: donors[0].id, name: 'donation1', description: 'desc1'},
+                              {donor_id: donors[0].id, name: 'donation2', description: 'desc2'},
+                              {donor_id: donors[0].id, name: 'donation3', description: 'desc3'},
+                              {donor_id: donors[0].id, name: 'donation4', description: 'desc4'},
+                              {donor_id: donors[0].id, name: 'donation5', description: 'desc5'},
+                              {donor_id: donors[0].id, name: 'donation6', description: 'desc6'},
+                              {donor_id: donors[0].id, name: 'donation7', description: 'desc7'},
+                              {donor_id: donors[0].id, name: 'donation8', description: 'desc8'},
+                              {donor_id: donors[0].id, name: 'donation9', description: 'desc9'},
+                              {donor_id: donors[0].id, name: 'donation10', description: 'desc10'},
+                              {donor_id: donors[0].id, name: 'donation11', description: 'desc11'},
+                              {donor_id: donors[0].id, name: 'donation12', description: 'desc12'},
+                              {donor_id: donors[0].id, name: 'donation13', description: 'desc13'},
+                            ])

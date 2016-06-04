2414af0b08dfc03ee6b7dc9d77d53f1560aae305
Enrique Delgado <delgado.enrique@gmail.com>
2016-06-04 15:13:49 -0500

    Fix deprecation warning.

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,6 @@ module FoodDonate
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.serve_static_assets = true
+    config.serve_static_files = true
   end
 end

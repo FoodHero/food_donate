580715ed088ae0fa452b8ceddc661ad6aa0e4051
rubygeek <nola@rubygeek.com>
2016-06-05 13:20:00 +0000

    added registration user name and changed donor table

--- spec/models/donation_spec.rb
+++ spec/models/donation_spec.rb
@@ -6,4 +6,9 @@ RSpec.describe Donation, type: :model do
   it "has a valid factory" do
     expect(donation).to be_valid
   end
+
+  it "has a donor notes field" do
+    dondation = Donation.create(name: "Test", donor_notes: "Hi Mom")
+    expect(donation.donor_notes).to eql("Hi Mom")
+  end
 end
